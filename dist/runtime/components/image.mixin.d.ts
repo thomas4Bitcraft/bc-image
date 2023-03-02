242b077cc1b4cdb329f1ecaa79144925a9c1c71c
Thomas Brandauer <thomas@bitcraft.at>
2023-03-02 12:31:12 +0100

    feat: build

---
+++ dist/runtime/components/image.mixin.d.ts
@@ -0,0 +1,1 @@
+export declare const imageMixin: import("vue").VueConstructor<import("vue").default<Record<string, any>, Record<string, any>, never, never, (event: string, ...args: any[]) => import("vue").default<Record<string, any>, Record<string, any>, never, never, any>>>;
---
+++ dist/runtime/components/nuxt-img.vue.d.ts
@@ -0,0 +1,2 @@
+declare const _default: import("vue/types/vue").ExtendedVue<import("vue").default<Record<string, any>, Record<string, any>, never, never, (event: string, ...args: any[]) => import("vue").default<Record<string, any>, Record<string, any>, never, never, any>>, unknown, unknown, unknown, unknown, {}, import("vue/types/v3-component-options").ComponentOptionsMixin, import("vue/types/v3-component-options").ComponentOptionsMixin>;
+export default _default;
